547bc3dcd6beaabd558e2b183a24be0db0cdd9a0
wep21 <border_goldenmarket@yahoo.co.jp>
2019-03-22 17:45:28 +0900

    fix shell script

--- ros/g30esli_interface.sh
+++ ros/g30esli_interface.sh
@@ -3,10 +3,13 @@
 sudo ip link set can0 type can bitrate 500000
 sudo ip link set can0 up
 
-cd
-candump -l can0 &
+if [ ! -d /home/autoware/ai-challenge/log ]; then
+   mkdir /home/autoware/ai-challenge/log
+fi
 
-source ../../../../../devel/setup.bash
+candump -L can0 > /home/autoware/ai-challenge/log/`date +%Y%m%d-%H-%M-%S`.log & 
+
+source /home/autoware/ai-challenge/Autoware/ros/devel/setup.bash
 roslaunch ymc g30esli_interface.launch
 
 sudo ip link set can0 down
